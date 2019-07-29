dc13e02442d709b6749f43f0cc15cf4bc2930178
Yasuhiro Suzuki <yasszu@yahoo.com>
2019-07-30 08:39:39 +0900

    edit property

--- src/main/scala/app/Consumer.scala
+++ src/main/scala/app/Consumer.scala
@@ -61,13 +61,18 @@ trait ConsumerModule extends Consumer[String, String] {
 
   val props: Properties = {
     val p = new Properties()
-    p.put("bootstrap.servers", "localhost:9092")
-    p.put("group.id", "test")
-    p.put("enable.auto.commit", "true")
+    p.setProperty("bootstrap.servers", "localhost:9092")
+    p.setProperty("group.id", "test")
+    p.setProperty("enable.auto.commit", "true")
+    p.setProperty("auto.commit.interval.ms", "1000")
     p
   }
 
-  val consumer = new KafkaConsumer[String, String](props, new StringDeserializer, new StringDeserializer)
+  val consumer =
+    new KafkaConsumer[String, String](
+      props,
+      new StringDeserializer,
+      new StringDeserializer)
 
   override def onSubscribe(records: Iterator[ConsumerRecord[String, String]]): Unit = {
     records.foreach { r =>
