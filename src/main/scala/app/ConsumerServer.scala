e9125674bd141fa061061d7719cbc8bae88e5bc6
Yasuhiro Suzuki <yasszu@yahoo.com>
2019-08-15 23:59:55 +0900

    handle errors

--- src/main/scala/app/ConsumerServer.scala
+++ src/main/scala/app/ConsumerServer.scala
@@ -4,36 +4,62 @@ import akka.Done
 import org.apache.kafka.common.errors.WakeupException
 import org.slf4j.{Logger, LoggerFactory}
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration.{FiniteDuration, _}
+import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.util.{Failure, Success, Try}
 
 trait ConsumerServer[K, V] {
   self =>
 
   val logger: Logger = LoggerFactory.getLogger(self.getClass)
 
-  val timeout: Long = 100
+  val timeout: FiniteDuration = 1000 milliseconds
 
   val topic: String
 
   val consumer: Consumer[K, V]
 
-  final def run()(implicit ec: ExecutionContext): Future[Done] = Future {
+  final def run()(implicit ec: ExecutionContext): Future[Done] = {
+    // Setup the consumer
     onStart()
+
+    // Stop the consumer when the VM exits
     sys.addShutdownHook {
+      logger.info("Stopping consumer...")
       onWakeup()
     }
-    try {
-      while (true) {
-        onSubscribe(consumer.poll(timeout))
+
+    val promise = Promise[Done]()
+
+    // Start subscribing messages
+    Future {
+      Try {
+        while (true) {
+          onSubscribe(consumer.poll(timeout.toMillis))
+        }
+        Done
+        // throw new IllegalStateException("Test")
+      } recoverWith {
+        case _: WakeupException => Success(Done)
+        case e =>
+          logger.error("Non fatal error occurred")
+          e.printStackTrace()
+          handleNonFatalError(e)
+      } match {
+        case Success(done) =>
+          onClose()
+          promise success done
+        case Failure(e) =>
+          onClose()
+          promise failure e
       }
-    } catch {
-      case _: WakeupException => logger.info("Stopping consumer...")
-      case e: Throwable => e.printStackTrace()
     }
-    finally {
-      onClose()
-    }
-    Done
+
+    promise.future
+  }
+
+  def handleNonFatalError(error: Throwable): Try[Done] = {
+    Failure(error)
   }
 
   protected def onStart(): Unit = {
--- src/main/scala/app/Main.scala
+++ src/main/scala/app/Main.scala
@@ -1,12 +1,17 @@
 package app
 
 import akka.actor.ActorSystem
+import org.slf4j.{Logger, LoggerFactory}
 
 import scala.concurrent.ExecutionContext
+import scala.util.{Failure, Success}
 
 object Main extends App {
+  self =>
 
-  val system = ActorSystem("kafka-consumer")
+  val logger: Logger = LoggerFactory.getLogger(self.getClass)
+
+  implicit val system: ActorSystem = ActorSystem("kafka")
 
   implicit val ec: ExecutionContext = system.dispatcher
 
@@ -14,8 +19,16 @@ object Main extends App {
 
   val postProducerServer = PostProducerServer()
 
-  postConsumerServer.run()
-
+  // Start a producer
   postProducerServer.run()
+
+  // Start a consumer
+  postConsumerServer.run() onComplete {
+    case Success(done) =>
+      logger.info("Stopped successfully")
+    case Failure(_) =>
+      logger.error("Unexpected error occurred")
+      system.terminate()
+  }
 
 }
